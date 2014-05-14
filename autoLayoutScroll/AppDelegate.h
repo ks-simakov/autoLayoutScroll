05fc34293247f7077591f2701cb14b61a51e5a3a
Simakov Konstantin <gigabytetheone@gmail.com>
2014-05-14 12:04:27 +0600

    Initial Commit

---
+++ autoLayoutScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  autoLayoutScroll
+//
+//  Created by Konstantin Simakov on 14/05/14.
+//  Copyright (c) 2014 Konstantin Simakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ autoLayoutScrollTests/autoLayoutScrollTests.m
@@ -0,0 +1,34 @@
+//
+//  autoLayoutScrollTests.m
+//  autoLayoutScrollTests
+//
+//  Created by Konstantin Simakov on 14/05/14.
+//  Copyright (c) 2014 Konstantin Simakov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface autoLayoutScrollTests : XCTestCase
+
+@end
+
+@implementation autoLayoutScrollTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
